49e3e08e2a5dfad8785a3037968f4afd2db9772a
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-01-21 09:50:23 +0300

    show reply form if the user is logged-in

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -15,31 +15,40 @@
                 @endcomponent
             </div>
 
-            <div class="col-md-8" style="margin-top:10px;">
-                @component('components.card',['title'=>'Reply'])
+            @auth
+                <div class="col-md-8" style="margin-top:10px;">
+                    @component('components.card',['title'=>'Reply'])
 
-                    @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul style="margin:0;">
-                                @foreach($errors->all() as $error)
-                                    <li>{{$error}}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                <ul style="margin:0;">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{$error}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
-                    <form method="post" action="{{route('replies.store',['id'=>$thread->id])}}">
+                        <form method="post" action="{{route('replies.store',['id'=>$thread->id])}}">
 
-                        {{csrf_field()}}
+                            {{csrf_field()}}
 
-                        <div class="form-group">
-                            <label for="body">Comment</label>
-                            <textarea class="form-control" name="body" id="id" rows="5"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                @endcomponent
-            </div>
+                            <div class="form-group">
+                                <label for="body">Comment</label>
+                                <textarea class="form-control" name="body" id="id" rows="5"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    @endcomponent
+                </div>
+            @endauth
+
+            @guest
+                <div class="col-md-8 text-center" style="margin-top:10px;padding: 30px;">
+                    Please <a href="{{route('login')}}">login</a> to add comment
+                </div>
+            @endguest
+
         </div>
 
         @foreach($thread->replies as $reply)
