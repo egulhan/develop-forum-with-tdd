2025a8ceb85e1fa7c586533853a0edbed6c9fd5f
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-02-06 10:11:40 +0300

    put old form data at thread creating

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -25,19 +25,22 @@
                             <select name="channel_id" id="channel_id" class="form-control">
                                 <option value="">Select</option>
                                 @foreach($channels as $channel)
-                                    <option value="{{$channel->id}}">{{$channel->name}}</option>
+                                    <option
+                                        value="{{$channel->id}}" {{old('channel_id')==$channel->id ? 'selected="selected"' : ''}}>
+                                        {{$channel->name}}
+                                    </option>
                                 @endforeach
                             </select>
                         </div>
 
                         <div class="form-group">
                             <label for="body">Title</label>
-                            <input type="text" name="title" id="title" class="form-control">
+                            <input type="text" name="title" id="title" class="form-control" value="{{old('title')}}">
                         </div>
 
                         <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea class="form-control" name="body" id="id" rows="5"></textarea>
+                            <textarea class="form-control" name="body" id="id" rows="5">{{old('body')}}</textarea>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Submit</button>
